d4fb525bb80f7582d01f13785c15176e249254a9
Md. Ismail Hossain <hossain.eee042@gmail.com>
2023-12-07 09:11:42 +0600

    basic setup

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constant.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tiktok clone',
+      /*   theme: ThemeData(
+        useMaterial3: false,
+      ), */
+      theme:
+          ThemeData.dark().copyWith(scaffoldBackgroundColor: backgroundColor),
+      home: Text("Hello"),
+    );
+  }
+}
