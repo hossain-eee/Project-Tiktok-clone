18934522f6cd9a482fb0596f84a6d6d9226340c5
Md. Ismail Hossain <hossain.eee042@gmail.com>
2023-12-08 17:48:29 +0600

    basic user register method created in Getx Controller

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,5 +1,21 @@
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'dart:io';
 
-class AuthCotroller extends GetxController{
-  
+import 'package:get/get.dart';
+
+class AuthCotroller extends GetxController {
+  //registering the user
+  void registerUser(
+      String userName, String email, String password, File? image) async {
+    //make sure every parameter has data
+    try {
+      if (userName.isNotEmpty &&
+          email.isNotEmpty &&
+          password.isNotEmpty &&
+          image != null) {
+        //save out user to our auth and firebase
+      }
+    } catch (e) {
+      Get.snackbar('Error Creating Account', e.toString());
+    }
+  }
 }
