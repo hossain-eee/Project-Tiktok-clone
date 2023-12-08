0c29c3561720dea1a6dfa6ad0fe2f7635391f8a4
Md. Ismail Hossain <hossain.eee042@gmail.com>
2023-12-08 18:48:46 +0600

    user model has been done

---
+++ lib/models/user.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class User {
+  String name;
+  String profilePhoto; // we are saving photo url not photo that's why string
+  String email;
+  String uid;
+  User(
+      {required this.name,
+      required this.profilePhoto,
+      required this.email,
+      required this.uid});
+
+  //convert to map
+  Map<String, dynamic> tojson() => {
+        "name": name,
+        "profilePhoto": profilePhoto,
+        "email": email,
+        "uid": uid,
+      };
+
+  // convert a DocumentSnapshot from Firestore into a User model
+  static User fromSnap(DocumentSnapshot snap) {
+    var snapShot = snap.data() as Map<String, dynamic>;
+    return User(
+      email: snapShot['email'],
+      profilePhoto: snapShot['profilePhoto'],
+      uid:snapShot['uid'],
+      name: snapShot['name'],
+    );
+  }
+}
